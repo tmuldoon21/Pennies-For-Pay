deb6a8085c8729ff77f6106d8ca6648e62b54bba
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2018-12-20 22:25:52 -0500

    Add files via upload

--- src/PenniesForPay.java
+++ src/PenniesForPay.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
-public class PenniesForPay {
+/**
+ * 
+ * @author Tyler Muldoon
+ *Pennies For Pay
+ */
+public class PenniesForPay 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner user = new Scanner(System.in);
+		System.out.print("Please enter the number of days you want to get paid for: ");
+		int days = user.nextInt();
+		double penny = 0.01;
+		double sum = 0;
+		for (int i = 0; i < days; i++)
+		{
+			sum += penny;
+			penny = penny * 2;	
+		}
+		System.out.println("In " + days + " days you earned $" + sum);
 
 	}
 
